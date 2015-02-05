5bd60001c5c8384dbfcaa9be91b588671ec0e31e
Ayinde Clarke <aclarke@asce.org>
2015-02-05 09:05:23 -0500

    initial push
    Signed-off-by: Ayinde Clarke <aclarke@asce.org>

---
+++ _vars.php
@@ -0,0 +1,13 @@
+<?php 
+
+// general variables
+$site_name = "Sample ASCE Conference";
+$conf_location = "Boston, Massachusetts";
+$conf_date = "April 3-5, 2015";
+$conf_venue = "Sheraton Boston Hotel & Hynes Convention Center";
+
+// menu items
+$home_link = '/';
+$sponsors_link = '/sponsors.php';
+
+?>
