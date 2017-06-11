152f01c3929f081b60fc30898bdd227c87498be2
Chris Harvey (work) <charvey@asce.org>
2017-06-11 09:15:36 -0400

    npm install gulp-pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require('gulp')
+var pug = require('gulp-pug')
+
+gulp.task('pug:index', function () {
+  return gulp.src('index-legacy.pug')
+    .pipe(pug({
+      basedir: './'
+    , locals: {
+      }
+    }))
+    .pipe(gulp.dest('./'))
+})
