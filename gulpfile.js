6e71fb9bfa004a885b60c1ab9e0660ddd5e6eeb9
Chris Harvey (work) <charvey@asce.org>
2017-06-11 09:18:56 -0400

    npm install gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var pug = require('gulp-pug')
+var less = require('gulp-less')
 
 gulp.task('pug:index', function () {
   return gulp.src('index-legacy.pug')
@@ -8,5 +9,17 @@ gulp.task('pug:index', function () {
     , locals: {
       }
     }))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('lessc:chewie', function () {
+  return gulp.src('styles/chewie.less')
+    .pipe(less())
+    .pipe(gulp.dest('./styles'))
+})
+
+gulp.task('lessc:legacy', function () {
+  return gulp.src('src/legacy.less')
+    .pipe(less())
     .pipe(gulp.dest('./'))
 })
